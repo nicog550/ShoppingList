1b0d1fa6292160862b332928e6daf9ba8cc66cad
yomesmo <nicog_cdc@hotmail.com>
2015-07-07 18:47:15 +0200

    implementada eliminación de categorías

--- exer35check/src/main/java/ltim/master/Categories.java
+++ exer35check/src/main/java/ltim/master/Categories.java
@@ -1,5 +1,6 @@
 package ltim.master;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.ContentValues;
@@ -8,17 +9,20 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import db.CategoriesContract;
 import db.CategoryDBHelper;
+import db.ElementContract;
+import db.ElementDBHelper;
+import db.ItemContract;
+import db.ItemDBHelper;
 
 public class Categories extends ListActivity {
 
@@ -110,5 +114,45 @@ public class Categories extends ListActivity {
         Intent intent = new Intent(this, Element.class);
         intent.putExtra("category", cat);
         startActivity(intent);
+    }
+
+    /**
+     * Es crida quan es pitja sobre el botó d'esborrar
+     * @param view
+     */
+    public void confirmRemovalClick(View view) {
+        View v = (View) view.getParent();
+        TextView taskTextView = (TextView) v.findViewById(R.id.categoryView);
+        final String cat = taskTextView.getText().toString();
+        final Activity currentActivity = this;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Eliminar una categoria");
+        builder.setMessage("Segur que voleu eliminar " + cat + "?");
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                ElementDBHelper helper = new ElementDBHelper(Categories.this);
+                SQLiteDatabase db = helper.getWritableDatabase();
+
+                // Eliminam els elements de la categoria
+                db.delete(ElementContract.TABLE,
+                        ElementContract.Columns.CATEGORY + "= ?",
+                        new String[] {cat});
+
+                // Eliminam la categoria
+                db.delete(CategoriesContract.TABLE,
+                        CategoriesContract.Columns.CATEGORY + "= ?",
+                        new String[]{cat});
+
+                String msg = "S'ha eliminat la categoria " + cat;
+                Toast.makeText(currentActivity, msg, Toast.LENGTH_SHORT).show();
+                updateCategoriesUI();
+            }
+        });
+
+        builder.setNegativeButton("Cancelar",null);
+
+        builder.create().show();
     }
 }
